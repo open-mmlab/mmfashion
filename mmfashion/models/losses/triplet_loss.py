9d2b6b91be83f2756f2c3c6b23d1bd4f8943ca6d
veralauee <xin.liu4@duke.edu>
2019-10-10 11:38:49 -0700

    triplet-loss

--- mmfashion/models/losses/triplet_loss.py
+++ mmfashion/models/losses/triplet_loss.py
@@ -10,7 +10,7 @@ class TripletLoss(nn.Module):
 
     def __init__(self, 
                  method='cosine',
-                 ratio=5,
+                 ratio=1,
                  margin=0.2, 
                  use_sigmoid=False, 
                  size_average=True):
@@ -23,17 +23,21 @@ class TripletLoss(nn.Module):
 
     def forward(self, anchor, pos, neg):
         if self.use_sigmoid:
-            anchor, pos, neg = F.sigmoid(anchor), F.sigmoid(pos), F.sigmoid(
-                neg)
-        if self.method == 'cosine':
-            anchor = torch.cos(anchor)
-            pos = torch.cos(pos)
-            neg = torch.cos(neg)
-            dist_pos = abs(anchor-pos)
-            dist_neg = abs(anchor-neg)
-            losses = self.ratio * F.relu(dist_pos - dist_neg + self.margin)
-        else:
+            anchor, pos, neg = F.sigmoid(anchor), F.sigmoid(pos), F.sigmoid(neg)
+        if self.method == 'cosine': # cosine similarity loss
+            loss_pos = F.cosine_embedding_loss(anchor, pos, 1, 
+                                               margin=self.margin, reduction=self.reduction)
+             
+            loss_neg = F.cosine_embedding_loss(anchor, neg, -1, 
+                                               margin=self.margin, reduction=self.reduction)
+            losses = loss_pos + loss_neg
+
+        else: # L2 loss
            dist_pos = (anchor - pos).pow(2).sum(1)
            dist_neg = (anchor - neg).pow(2).sum(1)
            losses = self.ratio * F.relu(dist_pos - dist_neg + self.margin)
-        return losses.mean() if self.size_average else losses.sum()
+           if self.size_average:
+              losses = losses.mean()
+           else:
+              losses = losses.sum()
+        return losses
