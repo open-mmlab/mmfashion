8f1ca23076325a0381d9f52c181f82e3c64c1182
veralauee <39462115+veralauee@users.noreply.github.com>
2019-10-17 09:58:59 -0700

    Delete upload.sh

--- data/upload.sh
+++
@@ -1,3 +0,0 @@
-./gdrive-linux-x64 upload /newDisk/users/liuxin/mmfashion-prerelease/mmfashion/checkpoint/Predict/vgg/global/latest.pth --parent 1q7xZJO373kI-l7jEVu0W2yBFpL_mh79t
-./gdrive-linux-x64 upload /newDisk/users/liuxin/mmfashion-prerelease/mmfashion/checkpoint/Predict/resnet/global/model_best.pth --parent 1RENkxpFZSpa7lqzGqSdXbwLPzFy232EI 
-./gdrive-linux-x64 upload /newDisk/users/liuxin/mmfashion-prerelease/mmfashion/checkpoint/Predict/resnet/roi/latest.pth --parent 1nOT2z236kdMrmtSfaWUiEA6roHJG28DT
