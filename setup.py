eea0ca1053483ad8f1c6903e38d9713fcb3568e7
lijiancheng0614 <lijiancheng0614@gmail.com>
2020-05-17 17:24:46 +0800

    Simplify versioning to upload pypi

--- setup.py
+++ setup.py
@@ -2,70 +2,9 @@
 """
 import os
 import re
-import subprocess
 import sys
-import time
 
 from setuptools import find_packages, setup
-
-MAJOR = 0
-MINOR = 4
-PATCH = 0
-SUFFIX = ''
-SHORT_VERSION = '{}.{}.{}{}'.format(MAJOR, MINOR, PATCH, SUFFIX)
-VERSION_FILE = 'mmfashion/version.py'
-
-
-def get_git_hash():
-    """Get git hash value.
-
-    Returns:
-        str: git hash value.
-    """
-
-    def _minimal_ext_cmd(cmd):
-        # construct minimal environment
-        env = {}
-        for key in ['SYSTEMROOT', 'PATH', 'HOME']:
-            value = os.environ.get(key)
-            if value is not None:
-                env[key] = value
-        # LANGUAGE is used on win32
-        env['LANGUAGE'] = 'C'
-        env['LANG'] = 'C'
-        env['LC_ALL'] = 'C'
-        out = subprocess.Popen(
-            cmd, stdout=subprocess.PIPE, env=env).communicate()[0]
-        return out
-
-    try:
-        out = _minimal_ext_cmd(['git', 'rev-parse', 'HEAD'])
-        sha = out.strip().decode('ascii')
-    except OSError:
-        sha = 'unknown'
-    return sha
-
-
-def get_hash():
-    """Get hash value.
-
-    Returns:
-        str: hash value.
-
-    Raises:
-        ImportError: import error.
-    """
-    if os.path.exists('.git'):
-        sha = get_git_hash()[:7]
-    elif os.path.exists(VERSION_FILE):
-        try:
-            from mmfashion.version import __version__
-            sha = __version__.split('+')[-1]
-        except ImportError:
-            raise ImportError('Unable to get git version')
-    else:
-        sha = 'unknown'
-    return sha
 
 
 def readme():
@@ -79,29 +18,17 @@ def readme():
     return content
 
 
-def write_version_py():
-    """Write version.py.
-    """
-    content = """# GENERATED VERSION FILE
-# TIME: {}
-
-__version__ = '{}'
-short_version = '{}'
-"""
-    sha = get_hash()
-    version = SHORT_VERSION + '+' + sha
-    with open(VERSION_FILE, 'w') as fid:
-        fid.write(content.format(time.asctime(), version, SHORT_VERSION))
-
-
-def get_version():
+def get_version(version_file='mmfashion/version.py'):
     """Get version.
+
+    Args:
+        version_file (str, optional): version file path.
 
     Returns:
         str: version string.
     """
-    with open(VERSION_FILE, 'r') as fid:
-        exec(compile(fid.read(), VERSION_FILE, 'exec'))
+    with open(version_file, 'r') as fid:
+        exec(compile(fid.read(), version_file, 'exec'))
     return locals()['__version__']
 
 
@@ -180,14 +107,14 @@ def parse_requirements(fname='requirements.txt', with_version=True):
 
 
 if __name__ == '__main__':
-    write_version_py()
     setup(
         name='mmfashion',
         version=get_version(),
         description='Open MMLab Fashion Toolbox',
         long_description=readme(),
+        long_description_content_type='text/markdown',
         author='OpenMMLab',
-        author_email='https://github.com/open-mmlab/mmfashion',
+        author_email='fake@email.com',
         keywords='computer vision, fashion',
         url='https://github.com/open-mmlab/mmfashion',
         classifiers=[
