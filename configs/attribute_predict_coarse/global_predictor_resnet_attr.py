57c6e9e7e32df8c2bfc1b096f62f7566fe3bcf83
Doğaç Eldenk <dogacel@gmail.com>
2021-02-02 16:43:17 +0300

    Fix config loss_attr runtime problem

--- configs/attribute_predict_coarse/global_predictor_resnet_attr.py
+++ configs/attribute_predict_coarse/global_predictor_resnet_attr.py
@@ -16,14 +16,18 @@ model = dict(
         inter_channels=[2048, 4096],
         outchannels=4096),
     attr_predictor=dict(
-        type='AttrPredictor', inchannels=4096, outchannels=attribute_num),
-    loss_attr=dict(
-        type='BCEWithLogitsLoss',
-        ratio=1,
-        weight=None,
-        size_average=None,
-        reduce=None,
-        reduction='mean'),
+        type='AttrPredictor',
+        inchannels=4096,
+        outchannels=attribute_num,
+        loss_attr=dict(
+            type='BCEWithLogitsLoss',
+            ratio=1,
+            weight=None,
+            size_average=None,
+            reduce=None,
+            reduction='mean',
+        ),
+    ),
     pretrained='checkpoint/resnet50.pth')
 
 pooling = 'Global'
